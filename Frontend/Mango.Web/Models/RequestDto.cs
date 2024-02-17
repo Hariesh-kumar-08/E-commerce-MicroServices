6696b520d1a770b08448cfd5795ac55d25df0cc1
ADMIN <ADMIN@DESKTOP-7RP912N>
2024-02-17 19:06:25 +0530

    Add project files.

---
+++ Frontend/Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.Security.AccessControl;
+using Mango.Web.Utility;
+using static Mango.Web.Utility.SD;
+
+public class RequestDto
+{
+    public ApiType ApiType { get; set; } = ApiType.GET;
+    public string Url { get; set; }
+    public object Data { get; set; }
+    public string AccessToken { get; set; }
+
+    //public ContentType ContentType { get; set; } = ContentType.Json;
+}
---
+++ Services/Mango.Serices.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Serices.CouponAPI.Models.Dto;
+using Mango.Serices.CouponAPI.Models;
+
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
